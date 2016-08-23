aa93616e18318c298119f694633aa4ce2a18ef1f
sheema1 <sheemasadia22@gmail.com>
2016-08-23 21:00:40 +1000

    first commit

---
+++ firstplugin.php
@@ -0,0 +1,61 @@
+<?php
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+/*
+Plugin Name: FirstPlugin
+Plugin URI: http://fyaconiello.github.com/wp-plugin-template
+Description: A simple WordPress plugin to show pakistan standard time
+Version: 1.0
+Author: Sheema Sadia
+Author URI: http://www.itfellow.com
+License: GPL2
+*/
+
+if(!class_exists('firstplugin'))
+{
+	class firstplugin
+	{
+		/**
+		 * Construct the plugin object
+		 */
+		public function __construct()
+		{
+			
+			add_action( 'init', 'pk_time_register_shortcode' );
+		} // END public function __construct
+
+		/**
+		 * Activate the plugin
+		 */
+		public static function activate()
+		{
+			// Do nothing
+		} // END public static function activate
+
+		/**
+		 * Deactivate the plugin
+		 */
+		public static function deactivate()
+		{
+			// Do nothing
+		} // END public static function deactivate
+				
+	} // END class firstplugin
+	
+		
+} // END if(!class_exists('firstplugin'))
+
+// register actions
+function show_pk_shortcode() {
+	$timezone = +5;
+	return gmdate("j M Y, g:i a", time() + 3600*($timezone+abs(date("I")-1)));
+}
+
+function pk_time_register_shortcode() {
+		
+	add_shortcode( 'pk-time', 'show_pk_shortcode' );
+}
+
+// call this plugin	
+$first_plugin = new firstplugin();
+
---
+++ readme.txt
@@ -0,0 +1,25 @@
+=== firstplugin ===
+Contributors: sheemasadia
+Donate link: http://sms4smile.com/
+Tags: pk time, pst
+Requires at least: 3.0.1
+Tested up to: 3.4
+Stable tag: 4.3
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+This plugin will show pakistan standard time
+
+== Description ==
+
+The purpose of this plugin is to provide facility to show pk time in post without knowing the difference
+
+== Installation ==
+
+This section describes how to install the plugin and get it working.
+
+e.g.
+
+1. Upload the plugin files to the `/wp-content/plugins/firstplugin` directory, or install the plugin through the WordPress plugins screen directly.
+1. Activate the plugin through the 'Plugins' screen in WordPress
+1. use short code in post [pk-time]
